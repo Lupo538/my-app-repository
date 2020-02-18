e41223596bb01602db7d27f7840dd84dc34945c4
Luca Contento <luca.contento-cic-it@ibm.com>
2020-02-18 16:05:52 +0100

    #3 test commit

--- src/CheckBoxComponent.jsx
+++ src/CheckBoxComponent.jsx
@@ -6,7 +6,7 @@ class CheckBoxComponent extends React.Component {
         return (
             <div><p>My Check Box</p>
                 <input type="checkbox" id="option1" /><label>First Option</label><br />
-                <input type="checkbox" id="box2" /><label>Second Option</label><br />
+                <input type="checkbox" id="op2" /><label>Second Option</label><br />
                 <input type="checkbox" id="box3" /><label>Third Option</label><br />
             </div>
         ); 
