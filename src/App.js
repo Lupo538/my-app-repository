b1c528e05aa16c68457bfc8fcc66b8c8e731c04c
Luca Contento <luca.contento-cic-it@ibm.com>
2020-02-18 14:30:51 +0100

    #1 create check box component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CheckBoxComponent from './CheckBoxComponent'
 
 function App() {
   return (
-    <div className="App">
+    <div className="CheckBoxComp">
+      <CheckBoxComponent></CheckBoxComponent>
+    </div>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
---
+++ src/CheckBoxComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class CheckBoxComponent extends React.Component {
+    render()
+    {
+        return (
+            <div><p>My Check Box</p>
+                <input type="checkbox" id="box1" /><label>First Option</label><br />
+                <input type="checkbox" id="box2" /><label>Second Option</label><br />
+                <input type="checkbox" id="box3" /><label>Third Option</label><br />
+            </div>
+        ); 
+    }
+}
+
+export default CheckBoxComponent;
